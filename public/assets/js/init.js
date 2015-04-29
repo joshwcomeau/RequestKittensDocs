80c8c15c3450e85af7a35faedde34d72700cba51
Joshua Comeau <joshwcomeau@gmail.com>
2015-04-29 10:47:53 -0400

    Added some logic so that the difference between development and production is 1 line in init.js, including the <base> tag.

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -1,10 +1,14 @@
 // The first JS file loaded.
+
+
 (function() {
-  var BASE_URL = 'http://requestkittens.com';
+  var developmentMode = true;
+
+  var BASE_URL = developmentMode ? 'http://localhost:3000' : 'http://requestkittens.com';
 
   window.reqKitConstants = {
-    catTemplate:      '/RequestKittensDocs/public/templates/cat.template.html',
-    fmTemplate:       '/RequestKittensDocs/public/templates/flash_message.template.html',
+    catTemplate:      developmentMode ? '/templates/cat.template.html' : '/RequestKittensDocs/public/templates/cat.template.html',
+    fmTemplate:       developmentMode ? '/templates/flash_message.template.html' : '/RequestKittensDocs/public/templates/flash_message.template.html',
     ApiCatIndex:      BASE_URL+'/cats',
     ApiCatCreate:     BASE_URL+'/cats',
     ApiEmotionIndex:  BASE_URL+'/emotions',
@@ -13,4 +17,9 @@
   };
 
   window.reqKitControllers = {};
+
+  if (!developmentMode) {
+    // Add our <base> tag to the top of our <head>
+    $("head").prepend('<base href="http://joshwcomeau.github.io/RequestKittensDocs/public/">');
+  }
 }());
